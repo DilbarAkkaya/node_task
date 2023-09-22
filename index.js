fae388b63f5942298133ce57c5000d9385a7fc83
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-09-23 00:22:00 +0300

    feat: try to output with readline

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const os = require('os');
 const fs = require('fs');
-const cp = require('child_process')
+const cp = require('child_process');
+const rl = require('readline');
 const LINUX = 'unixOS';
 const WINDOWS = 'windows';
 const LINUX_COMMAND = 'ps -A -o %cpu,%mem,comm | sort -nr | head -n 1';
@@ -9,16 +10,22 @@ const logFile = 'activityMonitor.log';
 //console.log(os.cpus());
 const execProcess = (command) => {
   const systemInfo = cp.exec(command, (error, stdout, stderr) => {
-    process.stdout.write(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
+    if(stdout) {
+     // rl.clearLine(process.stdout, 0)
+      //rl.cursorTo(process.stdout, 0, null)
+    //  process.stdout.write(text)
+      process.stdout.write(`${stdout}`);
+      
+    }
 
     if (error) {
       console.error(`error: ${error}`);
     }
   });
+
   systemInfo.on('exit', ()=>{
     setTimeout(execProcess, 100, command)
-  })
+  });
 }
 execProcess(WINDOWS_COMMAND)
 
