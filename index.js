16bb91037bb7dd48f0eaac830beddd3b57e21563
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-09-24 02:07:00 +0300

    feat: implement logfile and create file

--- index.js
+++ index.js
@@ -8,14 +8,22 @@ const LINUX_COMMAND = 'ps -A -o %cpu,%mem,comm | sort -nr | head -n 1';
 const WINDOWS_COMMAND = 'powershell "Get-Process | Sort-Object CPU -Descending | Select-Object -Property Name, CPU, WorkingSet -First 1 | ForEach-Object { $_.Name + \' \' + $_.CPU + \' \' + $_.WorkingSet }"';
 const logFile = 'activityMonitor.log';
 //console.log(os.cpus());
+function appendFile(info) {
+  const time = Math.floor(Date.now() / 1000);
+  if (fs.existsSync(logFile)) {
+    fs.appendFileSync(logFile, `${time} : ${info}\n`)
+  } else {
+    fs.writeFileSync(logFile, `${time} : ${info}\n`)
+  }
+};
 const execProcess = (command) => {
   const systemInfo = cp.exec(command, (error, stdout, stderr) => {
-    if(stdout) {
-     // rl.clearLine(process.stdout, 0)
+    if (stdout) {
+      // rl.clearLine(process.stdout, 0)
       //rl.cursorTo(process.stdout, 0, null)
-    //  process.stdout.write(text)
+      //  process.stdout.write(text)
       process.stdout.write(`${stdout}`);
-      
+      return stdout
     }
 
     if (error) {
@@ -23,8 +31,14 @@ const execProcess = (command) => {
     }
   });
 
-  systemInfo.on('exit', ()=>{
-    setTimeout(execProcess, 100, command)
+  systemInfo.on('exit', () => {
+    setTimeout(() => { execProcess(command) }, 100);
+
+    setInterval(() => {
+      const mostCPUProcess = cp.execSync(command);
+
+      appendFile(mostCPUProcess)
+    }, 1000)
   });
 }
 execProcess(WINDOWS_COMMAND)
