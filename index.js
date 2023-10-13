37cfa54cee1638f7a00d46d86c0a1f1f4de054d4
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-10-13 13:52:18 +0300

    feat: implement other methods

--- index.js
+++ index.js
@@ -8,23 +8,23 @@ function c2() {
 }
 
 class EventEmitter {
-  constructor(){
+  constructor() {
     this.listeners = {}
   }
 
- 
+
   addListener(eventName, fn) {
-    this.listeners[eventName] = this.listeners[eventName] || []; 
+    this.listeners[eventName] = this.listeners[eventName] || [];
     this.listeners[eventName].push(fn);
   }
-  
+
   on(eventName, fn) {
-    this.listeners[eventName] = this.listeners[eventName] || []; 
-    this.listeners[eventName].push(fn); 
+    this.listeners[eventName] = this.listeners[eventName] || [];
+    this.listeners[eventName].push(fn);
   }
-  emit(eventName){
-    if(this.listeners[eventName]) {
-      this.listeners[eventName].forEach(function(fn) {
+  emit(eventName) {
+    if (this.listeners[eventName]) {
+      this.listeners[eventName].forEach(function (fn) {
         fn()
       });
     }
@@ -32,28 +32,37 @@ class EventEmitter {
   removeListener(eventName, fn) {
     this.listeners[eventName] = this.listeners[eventName].filter(item => item !== fn);
   }
-   
+
   off(eventName, fn) {
     this.listeners[eventName] = this.listeners[eventName].filter(item => item !== fn);
   }
   once(eventName, fn) {
-    
+    this.listeners[eventName] = this.listeners[eventName] || [];
+    const onceWrapper = function () {
+      fn();
+      this.removeListener(eventName, onceWrapper);
+    }
+    this.listeners[eventName].push(onceWrapper);
   }
- /*
 
- 
-  once(eventName, fn) {}
- 
-  listenerCount(eventName) {}
- 
-  rawListeners(eventName) {} */
- }
- const myEmitter = new EventEmitter();
- myEmitter.addListener('eventOne', c1);
- myEmitter.on('eventOne', c2);
- myEmitter.removeListener('eventOne', c1);
- myEmitter.on('eventOne', c1);
- myEmitter.off('eventOne', c1);
- myEmitter.emit('eventOne');
- 
+  listenerCount(eventName) {
+    if (this.listeners[eventName]) {
+      return this.listeners[eventName].length;
+    }
+    return 0;
+  }
+  rawListeners(eventName) {
+    return this.listeners[eventName];
+  }
+}
 
+const myEmitter = new EventEmitter();
+myEmitter.addListener('eventOne', c1);
+myEmitter.on('eventOne', c2);
+myEmitter.removeListener('eventOne', c1);
+myEmitter.on('eventOne', c1);
+myEmitter.off('eventOne', c1);
+myEmitter.listenerCount('eventOne');
+console.log(myEmitter.listenerCount('eventOne'));
+console.log(myEmitter.rawListeners('eventOne'));
+myEmitter.emit('eventOne');
