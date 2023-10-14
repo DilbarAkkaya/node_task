bdd5620e3e4cd077a1772d8910fdd122c301f7fb
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-10-14 12:17:00 +0300

    feat: implement task 2

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ class EventEmitter {
   }
   once(eventName, fn) {
     this.listeners[eventName] = this.listeners[eventName] || [];
-    const onceWrapper = function () {
+    const onceWrapper = () => {
       fn();
       this.removeListener(eventName, onceWrapper);
     }
@@ -66,3 +66,42 @@ myEmitter.listenerCount('eventOne');
 console.log(myEmitter.listenerCount('eventOne'));
 console.log(myEmitter.rawListeners('eventOne'));
 myEmitter.emit('eventOne');
+
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Response isnt ok');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('error:', error);
+    throw error;
+  }
+}
+
+class WithTime extends EventEmitter {
+  async execute(asyncFunc, ...args) {
+    this.emit('begin');
+    console.log('Starting...');
+    let startingTime = Date.now();
+    let data = await asyncFunc(...args);
+    let endingTime = Date.now();
+    this.emit('end', endingTime - startingTime);
+    console.log(`Ending with ${endingTime - startingTime} ms`);
+    this.emit('data', data);
+    console.log('Data: ', data);
+  }
+}
+
+const withTime = new WithTime();
+withTime.on('begin', function beginHandler() {
+  console.log('About to execute')
+});
+withTime.on('end', function endHandler() {
+  console.log('Done with execute')
+});
+console.log(withTime.rawListeners("end"));
+let url = 'https://jsonplaceholder.typicode.com/posts/1';
+withTime.execute(fetchData, url);
