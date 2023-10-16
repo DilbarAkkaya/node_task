1607db5cffdcd9b7d497698236e91a9ea415984a
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-10-16 16:52:08 +0300

    feat: implement writing to text file

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const writeStream = fs.createWriteStream('text.txt', { flags: 'a' });
 
 readStream.on('data', (partsOfData) => {
   const lines = partsOfData.split('\n');
+  console.log(lines, 'these are lines')
   lines.forEach(async line => {
+    console.log(line)
     try{
-      if(line.length>0){
-        const json = await csv().fromString(line);
-        console.log(json)
+      if(line.trim().length > 0){
+      const json = await csv().fromString(partsOfData);
+        console.log('bbbb', json)
         writeStream.write(JSON.stringify(json));
-        console.log(JSON.stringify(json))
+       // console.log(JSON.stringify(json))
       }
     } catch (err) {
       console.log(err)
