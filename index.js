9a11bc750e77365d4fc59ba28c7117bd41dd4ad2
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-10-16 18:54:04 +0300

    feat: implement with writing all data

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const csv = require('csvtojson');
 
 const readStream = fs.createReadStream('file.csv', 'utf8');
 const writeStream = fs.createWriteStream('text.txt', { flags: 'a' });
+let allData = '';
+let result = [];
 
-readStream.on('data', (partsOfData) => {
+readStream.on('data', async (partsOfData) => {
+  allData += partsOfData;
+  try {
+    const jsonArray = await csv().fromString(allData);
+    for (const item of jsonArray) {
+      result.push(item);
+    }
+    writeStream.write(JSON.stringify(result));
+    allData = '';
+  } catch (err) {
+    console.log('error', err);
+  }
+});
+/* readStream.on('data', (partsOfData) => {
   const lines = partsOfData.split('\n');
   console.log(lines, 'these are lines')
   lines.forEach(async line => {
@@ -13,11 +28,14 @@ readStream.on('data', (partsOfData) => {
       if(line.trim().length > 0){
       const json = await csv().fromString(partsOfData);
         console.log('bbbb', json)
-        writeStream.write(JSON.stringify(json));
+        json.forEach((js) => {
+          writeStream.write(JSON.stringify(js) + '\n');
+        });
+      }
        // console.log(JSON.stringify(json))
       }
-    } catch (err) {
+     catch (err) {
       console.log(err)
     }
   });
-})
+}) */
